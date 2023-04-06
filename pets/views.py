4bfd97384d8dfd482cd785287a8722bef8f62a78
leticiamontilha <leticiammontilha@hotmail.com>
2023-04-06 10:28:30 -0300

    feat: tarefa 4 finalizada

--- pets/views.py
+++ pets/views.py
@@ -5,6 +5,7 @@ from .serializers import PetSerializer
 from traits.models import Trait
 from groups.models import Group
 from rest_framework.pagination import PageNumberPagination
+from django.shortcuts import get_object_or_404
 
 # Create your views here.
 
@@ -42,6 +43,17 @@ class PetViews(APIView, PageNumberPagination):
    
 
     def get(self, request:Request) -> Response:
+        traits_data = request.query_params.get("trait", None)
+
+        if traits_data:
+            pets = Pet.objects.filter(traits__name=traits_data)
+
+            result_page = self.paginate_queryset(pets, request, view=self)
+            
+            serializer = PetSerializer(result_page, many=True)
+
+            return self.get_paginated_response(serializer.data)
+        
         pets = Pet.objects.all()
         result_page = self.paginate_queryset(pets, request)
 
@@ -51,5 +63,55 @@ class PetViews(APIView, PageNumberPagination):
 
 
 class PetDetaisView(APIView):
-    ...
-   
+    def get(self, request:Request, pet_id: int):
+        pet = get_object_or_404(Pet, id=pet_id)
+        serializer = PetSerializer(instance=pet)
+
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    
+
+    def delete(self, request:Request, pet_id= int) -> Response:
+        pet = get_object_or_404(Pet, id=pet_id)
+        pet.delete()
+
+        return Response(status=status.HTTP_204_NO_CONTENT)
+    
+
+    def patch(self, request:Request, pet_id):
+        pet = get_object_or_404(Pet, id=pet_id)
+
+        serializer = PetSerializer(data=request.data, partial=True)
+        serializer.is_valid(raise_exception=True)
+
+        group_data = serializer.validated_data.pop("group", None)
+        traits_data = serializer.validated_data.pop("traits", None)
+
+        if group_data:
+            try:
+                new_group = Group.objects.get(
+                    scientific_name=group_data["scientific_name"]
+                )
+                pet.group = new_group
+
+            except Group.DoesNotExist:
+                add_group = Group.objects.create(**group_data)
+                pet.group = add_group
+        
+        if traits_data:
+            for traits in traits_data:
+                 traits_obj = Trait.objects.filter(
+                    name__iexact=traits["name"]
+                ).first()
+                 
+            if not traits_obj:
+                traits_obj = Trait.objects.create(**traits)
+
+            pet.traits.add(traits_obj)
+        
+        for key, value in serializer.validated_data.items():
+            setattr(pet, key, value)
+        
+        pet.save()
+        serializer = PetSerializer(pet)
+
+        return Response(serializer.data, status=status.HTTP_200_OK)
